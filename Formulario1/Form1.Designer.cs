93487aea474694cba5bc032bef6a20882600f243
Matias <mati.j.martinez@gmail.com>
2024-08-02 18:32:48 -0300

    Agregue btnApretar

--- Formulario1/Form1.Designer.cs
+++ Formulario1/Form1.Designer.cs
@@ -37,80 +37,100 @@ namespace Formulario1
             this.BEliminar = new System.Windows.Forms.Button();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.BSalir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LApellido
             // 
             this.LApellido.AutoSize = true;
-            this.LApellido.Location = new System.Drawing.Point(89, 138);
+            this.LApellido.Location = new System.Drawing.Point(67, 112);
+            this.LApellido.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LApellido.Name = "LApellido";
-            this.LApellido.Size = new System.Drawing.Size(58, 17);
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
             this.LApellido.TabIndex = 0;
             this.LApellido.Text = "Apellido";
             // 
             // LNombre
             // 
             this.LNombre.AutoSize = true;
-            this.LNombre.Location = new System.Drawing.Point(89, 195);
+            this.LNombre.Location = new System.Drawing.Point(67, 158);
+            this.LNombre.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LNombre.Name = "LNombre";
-            this.LNombre.Size = new System.Drawing.Size(58, 17);
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
             this.LNombre.TabIndex = 1;
             this.LNombre.Text = "Nombre";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(217, 140);
+            this.textBox1.Location = new System.Drawing.Point(163, 114);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(199, 22);
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(217, 190);
+            this.textBox3.Location = new System.Drawing.Point(163, 154);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(199, 22);
+            this.textBox3.Size = new System.Drawing.Size(150, 20);
             this.textBox3.TabIndex = 5;
             // 
             // BGuardar
             // 
-            this.BGuardar.Location = new System.Drawing.Point(92, 298);
+            this.BGuardar.Location = new System.Drawing.Point(69, 242);
+            this.BGuardar.Margin = new System.Windows.Forms.Padding(2);
             this.BGuardar.Name = "BGuardar";
-            this.BGuardar.Size = new System.Drawing.Size(75, 23);
+            this.BGuardar.Size = new System.Drawing.Size(56, 19);
             this.BGuardar.TabIndex = 6;
             this.BGuardar.Text = "Guardar";
             this.BGuardar.UseVisualStyleBackColor = true;
             // 
             // BEliminar
             // 
-            this.BEliminar.Location = new System.Drawing.Point(236, 298);
+            this.BEliminar.Location = new System.Drawing.Point(177, 242);
+            this.BEliminar.Margin = new System.Windows.Forms.Padding(2);
             this.BEliminar.Name = "BEliminar";
-            this.BEliminar.Size = new System.Drawing.Size(75, 23);
+            this.BEliminar.Size = new System.Drawing.Size(56, 19);
             this.BEliminar.TabIndex = 7;
             this.BEliminar.Text = "Eliminar";
             this.BEliminar.UseVisualStyleBackColor = true;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(479, 84);
+            this.textBox4.Location = new System.Drawing.Point(359, 68);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(290, 237);
+            this.textBox4.Size = new System.Drawing.Size(218, 193);
             this.textBox4.TabIndex = 8;
             // 
             // BSalir
             // 
-            this.BSalir.Location = new System.Drawing.Point(584, 366);
+            this.BSalir.Location = new System.Drawing.Point(438, 297);
+            this.BSalir.Margin = new System.Windows.Forms.Padding(2);
             this.BSalir.Name = "BSalir";
-            this.BSalir.Size = new System.Drawing.Size(94, 35);
+            this.BSalir.Size = new System.Drawing.Size(70, 28);
             this.BSalir.TabIndex = 9;
             this.BSalir.Text = "SALIR";
             this.BSalir.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 40);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "APRETAME";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.BSalir);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.BEliminar);
@@ -119,6 +139,7 @@ namespace Formulario1
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.LNombre);
             this.Controls.Add(this.LApellido);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Formulario1";
             this.ResumeLayout(false);
@@ -136,6 +157,7 @@ namespace Formulario1
         private System.Windows.Forms.Button BEliminar;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button BSalir;
+        private System.Windows.Forms.Button button1;
     }
 }
 
